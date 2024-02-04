78b113d50de7b63f633c3a3b733ac1c9c605ea5f
Nicel193 <nicelgoog@gmail.com>
2024-02-04 14:35:48 +0200

    Add overflow detector

---
+++ Assets/Code/Editor/OverflowDetectorEditor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(OverflowDetector))]
+    public class OverflowDetectorEditor : UnityEditor.Editor
+    {
+        [DrawGizmo(GizmoType.Active | GizmoType.Pickable | GizmoType.NonSelected)]
+        public static void DrawCustomGizmo(OverflowDetector overflowDetector, GizmoType gizmoType)
+        {
+            Vector3 position = overflowDetector.transform.position;
+            Collider2D collider2D = overflowDetector.GetComponent<Collider2D>();
+            
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube(position, collider2D.bounds.size);
+        }
+    }
+}
--- Assets/Code/Runtime/Extensions/GameExtensions.cs
+++ Assets/Code/Runtime/Extensions/GameExtensions.cs
@@ -1,13 +1,25 @@
-﻿using Code.Runtime.Infrastructure.States;
+﻿using System;
+using Code.Runtime.Infrastructure.States;
 
 namespace Code.Runtime.Extensions
 {
     public static class GameExtensions
     {
-        public static ShapeSize NextSize(this ShapeSize shapeSize) 
-            => (ShapeSize) (((int) shapeSize) + 1);
+        public static ShapeSize NextSize(this ShapeSize shapeSize)
+        {
+            int nextSize = (((int) shapeSize) + 1);
+
+            if (Enum.GetNames(typeof(ShapeSize)).Length < nextSize)
+            {
+                nextSize = 0;
+            }
+
+            return (ShapeSize) nextSize;
+        }
 
         public static int Index(this SceneName sceneName)
-            => (int) sceneName;
+        {
+            return (int) sceneName;
+        }
     }
 }
