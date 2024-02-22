c79157d6c2d456060f246fd3ab2dadf3858b0976
admin <nicelgoog@gmail.com>
2024-02-23 00:31:13 +0200

    add missing files

---
+++ Assets/Code/Editor/GameplayAssetsConfigEditor.cs
@@ -0,0 +1,33 @@
+﻿using Code.Runtime.Configs;
+using UnityEditor;
+using UnityEngine;
+
+namespace Code.Editor
+{
+    [CustomEditor(typeof(GameplayConfig))]
+    public class GameplayAssetsConfigEditor : UnityEditor.Editor
+    {
+        private const string SpawnPointTag = "SpawnPoint";
+        
+        private GameplayConfig _gameplayConfig;
+
+        void OnEnable()
+        {
+            _gameplayConfig = (GameplayConfig)target;
+        }
+
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            
+            if (GUILayout.Button("Find Spawn Point Position"))
+            {
+                GameObject spawnPoint = GameObject.FindGameObjectWithTag(SpawnPointTag);
+
+                if(spawnPoint == null) return;
+                
+                _gameplayConfig.SpawnPointPosition = spawnPoint.transform.position;
+            }
+        }
+    }
+}
